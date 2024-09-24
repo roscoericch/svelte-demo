e05ee8f802eeaeb7bc6dfb5db5889e621c737228
sikiru razak <sikirurazak1@gmail.com>
2024-09-24 13:16:03 +0100

    update prop binding and reactive variables

---
+++ src/components/Age.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let age:number = 10;
+    $: factoAge = age * 10
+</script>
+
+<section>
+<h1>my Age is {age}</h1>
+<h1>facto Age is {factoAge}</h1>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Age from "../components/Age.svelte";
+    let age:number = 21;
+    function add() {
+        age += 1
+    }
+    function minus() {
+        age -= 1
+    }
+</script>
+
+<main class="h-full flex flex-col items-center justify-center">
+    <Age {age} />
+    <input bind:value={age} type="number" />
+    <button on:click={add}>+</button>
+    <button on:click={minus}>-</button>
+</main>
